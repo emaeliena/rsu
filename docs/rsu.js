baed8227096f26b80aa5a707b8e494936d87e826
Artur Malinowski <emaeliena@gmail.com>
2017-03-07 21:29:49 +0100

    jQuery UI datepicker added

--- docs/rsu.js
+++ docs/rsu.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
-    $.ajax({
-        url: 'https://rsuapi.herokuapp.com/v1/rates/USD'
-    }).done(function(data) {
-        $('table#rates tbody').empty();
-        $('table#rates tbody').append('<tr class="dates"></tr>');
-        $('table#rates tbody').append('<tr class="values"></tr>');
-        $.each(data, function(index, value) {
-            var date = new Date(value.date);
-            $('table#rates tbody tr.dates').prepend('<th>' + date.toISOString().slice(0, 10) + '</th>');
-            $('table#rates tbody tr.values').prepend('<td>' + value.value + '</td>');
+
+    var queryRates = function(date) {
+        var url = 'http://127.0.0.1:5000/v1/rates/USD';
+        if (date != '') {
+            url += '?date=' + date;
+        }
+        $.ajax({
+            url: url
+        }).done(function(data) {
+            var lastDate = new Date(data[0].date).toISOString().slice(0, 10);
+            var $searchDateWidget = $('#search_date');
+            if ($searchDateWidget.val() == '') {
+                $searchDateWidget.val(lastDate);
+            }
+            $('table#rates tbody').empty();
+            $('table#rates tbody').append('<tr class="dates"></tr>');
+            $('table#rates tbody').append('<tr class="values"></tr>');
+            $.each(data, function(index, value) {
+                var date = new Date(value.date);
+                $('table#rates tbody tr.dates').prepend('<th>' + date.toISOString().slice(0, 10) + '</th>');
+                $('table#rates tbody tr.values').prepend('<td>' + value.value + '</td>');
+            });
         });
+    };
+
+    $('#search_date').change(function(event) {
+        queryRates(event.target.value);
     });
+
+    $('#search_date').datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+
+    queryRates('');
 });
