71afef58c0be86c01cc69f719260815fe0b3ef5f
Artur Malinowski <emaeliena@gmail.com>
2017-03-07 07:49:36 +0100

    Checking if mongo connection is working

--- api.py
+++ api.py
@@ -1,7 +1,19 @@
+import datetime.datetime as dt
+import os
+
 from flask import Flask, jsonify
+from pymongo import MongoClient
+
+
+MONGODB_URI = os.getenv('MONGODB_URI')
 
 
 app = application = Flask(__name__)
+mongo_conn = MongoClient(MONGODB_URI)
+mongo = mongo_conn.get_default_database()
+
+
+mongo.connections.insert({'dt': dt.now()})
 
 
 @app.route("/")
